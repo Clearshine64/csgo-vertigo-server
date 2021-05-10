d042c4d5fa2519da4f0be00b4a327fccf7589b94
unknown <Administrator@DESKTOP-ND8C6NK.mana>
2021-05-10 15:57:59 +0300

    email

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,9 +1,12 @@
 const Logo = (props) => (
+  <>
   <img
     alt="Logo"
     src="/static/logo.svg"
     {...props}
   />
+  <span style={{color : 'white'}}>Email Address : nikita.afanasev2020@gmail.com</span>
+  </>
 );
 
 export default Logo;
